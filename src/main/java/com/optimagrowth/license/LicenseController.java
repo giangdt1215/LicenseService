b659b66e797a69f293d47a5095d10cb710930202
Dang Giang <gianggiang1512@gmail.com>
2022-05-24 22:16:06 +0700

    Add HATEOAS links to getLicense in LicenseController

--- src/main/java/com/optimagrowth/license/LicenseController.java
+++ src/main/java/com/optimagrowth/license/LicenseController.java
@@ -3,6 +3,7 @@ package com.optimagrowth.license;
 import com.optimagrowth.license.model.License;
 import com.optimagrowth.license.service.LicenseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,6 +20,21 @@ public class LicenseController {
     public ResponseEntity<License> getLicense(@PathVariable("organizationId") String organizationId,
                                               @PathVariable("licenseId") String licenseId){
         License license = licenseService.getLicense(licenseId, organizationId);
+
+        //Add link HATEOAS
+        license.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(LicenseController.class)
+                .getLicense(organizationId, license.getLicenseId()))
+                .withSelfRel(),
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(LicenseController.class)
+                .createLicense(organizationId, license, null))
+                .withRel("createLicense"),
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(LicenseController.class)
+                .updateLicense(organizationId, license, null))
+                .withRel("updateLicense"),
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(LicenseController.class)
+                .deleteLicense(organizationId, license.getLicenseId(), null))
+                .withRel("deleteLicense")
+        );
         return ResponseEntity.ok(license);
     }
 
