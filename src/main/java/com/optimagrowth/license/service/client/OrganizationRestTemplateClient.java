f56078858571f77e0e562956bc27a4f5e9f23454
Dang Giang <gianggiang1512@gmail.com>
2022-06-28 00:38:57 +0700

    Change default RestTemplate bean to KeycloakRestTemplate bean

--- src/main/java/com/optimagrowth/license/service/client/OrganizationRestTemplateClient.java
+++ src/main/java/com/optimagrowth/license/service/client/OrganizationRestTemplateClient.java
@@ -1,24 +1,24 @@
 package com.optimagrowth.license.service.client;
 
 import com.optimagrowth.license.model.Organization;
+import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
 @Component
 public class OrganizationRestTemplateClient {
 
     @Autowired
-    RestTemplate restTemplate;
+    private KeycloakRestTemplate restTemplate;
 
     public Organization getOrganization(String organizationId){
         ResponseEntity<Organization> restExchange = restTemplate.exchange(
-                "http://organization-service/v1/organization/{organizationId}",
+                "http://gateway:8072/organization/v1/organization/{organizationId}",
                 HttpMethod.GET, null,
-                Organization.class, organizationId
-        );
+                Organization.class, organizationId);
+
         return restExchange.getBody();
     }
 }
