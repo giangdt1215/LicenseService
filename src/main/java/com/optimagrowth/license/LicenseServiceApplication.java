c49d55d8845acb8275aeecf167f33a64ff35e92c
Dang Giang <gianggiang1512@gmail.com>
2022-06-30 23:23:19 +0700

    Add binding Sink interface for receiving events

--- src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
+++ src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
@@ -1,12 +1,18 @@
 package com.optimagrowth.license;
 
+import com.optimagrowth.license.events.model.OrganizationChangeModel;
 import com.optimagrowth.license.utils.UserContextInterceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
@@ -22,7 +28,16 @@ import java.util.Locale;
 @RefreshScope
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableBinding(Sink.class)
 public class LicenseServiceApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(LicenseServiceApplication.class);
+
+    @StreamListener(Sink.INPUT)
+    public void loggerSink(OrganizationChangeModel orgChange){
+        logger.debug("Received an {} event for organization id {}",
+                orgChange.getAction(), orgChange.getOrganizationId());
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(LicenseServiceApplication.class, args);
