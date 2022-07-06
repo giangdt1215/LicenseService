fa1237438f20502b26e6aaa0ce28bd6fc9f3c61c
Dang Giang <gianggiang1512@gmail.com>
2022-07-06 21:28:03 +0700

    Add feature Sink.class as Spring Cloud Stream Kafka StreamListener

--- src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
+++ src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
@@ -1,20 +1,27 @@
 package com.optimagrowth.license;
 
+import com.optimagrowth.license.config.ServiceConfig;
 import com.optimagrowth.license.events.model.OrganizationChangeModel;
 import com.optimagrowth.license.utils.UserContextInterceptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.LocaleResolver;
@@ -28,10 +35,14 @@ import java.util.Locale;
 @RefreshScope
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableEurekaClient
 @EnableBinding(Sink.class)
 public class LicenseServiceApplication {
 
     private static final Logger logger = LoggerFactory.getLogger(LicenseServiceApplication.class);
+
+    @Autowired
+    private ServiceConfig serviceConfig;
 
     @StreamListener(Sink.INPUT)
     public void loggerSink(OrganizationChangeModel orgChange){
@@ -70,5 +81,20 @@ public class LicenseServiceApplication {
             restTemplate.setInterceptors(interceptors);
         }
         return restTemplate;
+    }
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory(){
+        String hostname = serviceConfig.getRedisServer();
+        int port = Integer.parseInt(serviceConfig.getRedisPort());
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(hostname, port);
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(){
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
     }
 }
